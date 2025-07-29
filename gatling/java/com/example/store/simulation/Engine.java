a8b81312d0ca43356263a96ebe75048f31d381fe
Satheesh <sound4peace@gmail.com>
2025-07-29 20:05:44 +0200

    Spotless Check

--- gatling/java/com/example/store/simulation/Engine.java
+++ gatling/java/com/example/store/simulation/Engine.java
@@ -10,7 +10,7 @@ public class Engine {
     private static final String baseEndpointUrl = "http://localhost:8080";
 
     // microk8s URL for the API endpoint, it may change based on NodePort assigned to the service
-    //private static final String baseEndpointUrl = "http://10.152.183.18:8080";
+    // private static final String baseEndpointUrl = "http://10.152.183.18:8080";
 
     public static final HttpProtocolBuilder httpProtocol = http.baseUrl(baseEndpointUrl)
             .acceptHeader("application/json")
