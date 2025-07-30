d8c6dc11a8d0ea966906d909e0dff6893a2fdcac
Satheesh <sound4peace@gmail.com>
2025-07-30 03:44:10 +0200

    Updated simulations

--- gatling/java/com/example/store/simulation/Engine.java
+++ gatling/java/com/example/store/simulation/Engine.java
@@ -7,10 +7,10 @@ import static io.gatling.javaapi.http.HttpDsl.http;
 public class Engine {
 
     // Localhost URL for the API endpoint, also when port forwarding is used
-    // private static final String baseEndpointUrl = "http://localhost:8080";
+    private static final String baseEndpointUrl = "http://localhost:8080";
 
     // Optional - microk8s URL for the API endpoint, it may change based on NodePort assigned to the service
-    private static final String baseEndpointUrl = "http://10.152.183.69:8080";
+    // private static final String baseEndpointUrl = "http://10.152.183.169:8080";
 
     public static final HttpProtocolBuilder httpProtocol = http.baseUrl(baseEndpointUrl)
             .acceptHeader("application/json")
